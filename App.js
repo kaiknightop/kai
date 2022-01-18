ffb8b846fbe995c809162afd02109e85bb027db6
kaiknightop <97940245+kaiknightop@users.noreply.github.com>
2022-01-18 08:57:15 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+import './App.css';
+
+
+function App() {
+  return (
+    
+    
+    
+    <div className="App">
+     <h1>Welcome to my website</h1>
+     <h2>My name is Kai Knight</h2>
+    
+     
+     
+
+    
+
+     <h3>I'm an Artist!</h3>
+     
+     
+     
+     
+     <img src="/images/anime-boruto-kawaki-boruto-wallpaper-preview.jpg" alt=""/>
+    
+     <div>
+       <b>click the link below</b>
+     </div>
+     
+     
+     
+     <u1>
+       <button>click me<select><option>More</option></select></button>
+
+
+     </u1>
+
+     <div>
+       <h1>Introduction</h1>
+       <b>My Art is an Explosion</b>
+       </div>
+       <div>
+       <c>Cower in fear to my art, :) </c>
+       <e>.My art welcomes the ultimate, you know?</e>
+       <f>yo, Let's get serious, so.. My name is Kai Knight </f>
+       <g>. I create fan art and post it on this website (My website lol)</g>
+       
+     </div>
+     
+     <u1>
+       <button>click me</button>
+
+
+     </u1>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Images(){
+    return(
+        <div>
+            <img src="./Images/anime-boruto-kawaki-boruto-wallpaper-preview" alt=""/>
+        </div>
+    )
+}
+
+
+export default Images;
+
